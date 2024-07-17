139ded48c71779f1cce0176acd57c1a7a6ea00e7
CodeDroid999 <johnmwega2002@gmail.com>
2024-07-17 17:35:15 +0300

    stereotyepin

--- api/index.py
+++ api/index.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return send_from_directory('.', 'index.html')
+
 
 @app.route('/about')
 def about():
-    return 'About'
+    return send_from_directory('.', 'about.html')
+
+
+@app.route('/blog')
+def blog():
+    return send_from_directory('.', 'blog.html')
+
+
+@app.route('/contact')
+def contact():
+    return send_from_directory('.', 'contact.html')
+
+
+@app.route('/faq')
+def faq():
+    return send_from_directory('.', 'faq.html')
+
+
+@app.route('/services')
+def services():
+    return send_from_directory('.', 'services.html')
+
+
+@app.route('/404')
+def error_404():
+    return send_from_directory('.', '404.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
